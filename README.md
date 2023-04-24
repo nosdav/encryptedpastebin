c10e7128d283daf059617ea2857d7b1b8c9d3bdc
Melvin Carvalho <melvincarvalho@gmail.com>
2023-04-24 19:33:00 +0200

    first release

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# encryptedpastebin
+
+
+<div align="center">  
+  <h1>encryptedpastebin</h1>
+</div>
+
+<div align="center">  
+<i>encryptedpastebin</i>
+</div>
+
+---
+
+<div align="center">
+<h4>Documentation</h4>
+</div>
+
+---
+
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/nosdav/encryptedpastebin/blob/gh-pages/LICENSE)
+[![npm](https://img.shields.io/npm/v/encryptedpastebin)](https://npmjs.com/package/encryptedpastebin)
+[![npm](https://img.shields.io/npm/dw/encryptedpastebin.svg)](https://npmjs.com/package/encryptedpastebin)
+[![Github Stars](https://img.shields.io/github/stars/nosdav/encryptedpastebin.svg)](https://github.com/nosdav/encryptedpastebin/)
+
+## Introduction
+
 encryptedpastebin
+
+## License
+
+- MIT
---
+++ app.js
@@ -0,0 +1,163 @@
+// app.js
+import { html, Component, render } from './js/spux.js'
+import { getPath, getQueryStringValue, loadFile, saveFile } from './util.js'
+import './js/dior.js'
+
+export class App extends Component {
+  constructor() {
+    super()
+    const serverUrl = getQueryStringValue('storage') || di.data.storage || 'https://nosdav.nostr.rocks'
+    const mode = getQueryStringValue('mode') || di.data.m || 'm'
+    const uri = getQueryStringValue('uri') || di.data.uri || 'paste.txt'
+    this.state = {
+      userPublicKey: null,
+      filename: uri,
+      contentType: null,
+      fileContent: '',
+      path: null,
+      downloadLink: '',
+      serverUrl: serverUrl,
+      mode: mode
+    }
+  }
+
+  // Add this function to your App component
+  toggleEncryption = async () => {
+    if (!this.state.userPublicKey) {
+      alert('Please login first');
+      return;
+    }
+
+    const { fileContent } = this.state;
+
+    try {
+      const decrypted = await window.nostr.nip04.decrypt(this.state.userPublicKey, fileContent);
+      this.setState({ fileContent: decrypted });
+    } catch (error) {
+      const encrypted = await window.nostr.nip04.encrypt(this.state.userPublicKey, fileContent)
+      this.setState({ fileContent: encrypted });
+    }
+  };
+
+  updateFileContent = (event) => {
+    this.setState({ fileContent: event.target.value })
+  }
+
+  updateDownloadLink = () => {
+    const { filename, userPublicKey, serverUrl, mode } = this.state
+    const path = getPath(serverUrl, userPublicKey, filename, mode)
+    this.setState({ downloadLink: path })
+  }
+
+  updateFilename = (event) => {
+    this.setState({ filename: event.target.value })
+  }
+
+  userLogin = async () => {
+    const userPublicKey = await window.nostr.getPublicKey()
+    console.log(`Logged in with public key: ${userPublicKey}`)
+    await this.setState({ userPublicKey: userPublicKey })
+    this.loadFile()
+  }
+
+  // Load the file content from the server
+  loadFile = async () => {
+    if (!this.state.userPublicKey) {
+      alert('Please login first');
+      return;
+    }
+
+    const { filename, userPublicKey, serverUrl, mode } = this.state;
+
+    const fileContent = await loadFile(serverUrl, userPublicKey, filename, mode);
+
+    if (fileContent) {
+      this.setState({ fileContent: fileContent });
+    }
+
+    this.updateDownloadLink();
+  };
+
+  save = async () => {
+    if (!this.state.userPublicKey) {
+      alert('Please login first');
+      return;
+    }
+
+    const { fileContent, filename, userPublicKey, serverUrl, mode } = this.state;
+
+    const success = await saveFile(serverUrl, userPublicKey, filename, mode, fileContent);
+
+    if (!success) {
+      alert('Error saving file');
+    }
+  };
+
+  render() {
+    const { userPublicKey, filename, fileContent, downloadLink } = this.state
+
+    return html`
+      <div id="container">
+        <h1>NosDAV</h1>
+        <label for="file-name"
+          >File${' '}
+          <a href="${downloadLink}" id="download" target="_blank">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+              <polyline points="7 10 12 15 17 10"></polyline>
+              <line x1="12" y1="15" x2="12" y2="3"></line></svg></a
+        ></label>
+        <input
+          type="text"
+          id="file-name"
+          name="file-name"
+          placeholder="Enter filename"
+          value="${filename}"
+          onInput="${event => {
+        this.updateFilename(event)
+        this.updateDownloadLink()
+      }}"
+        />
+        
+        <label for="content">Enter Text Below</label>
+
+        <textarea
+          id="file-content"
+          placeholder="Login First"
+          name="content"
+          rows="10"
+          value="${fileContent}"
+          onInput="${this.updateFileContent}"
+        ></textarea>
+        <div>
+          ${userPublicKey
+        ? html`
+                <button id="save" onClick="${this.save}">Save</button>
+                <button id="load" onClick="${this.loadFile}">
+                  Load
+                </button>
+                <button id="load" onClick="${this.toggleEncryption}">
+                  🔒
+                </button>
+              `
+        : html` <button id="login" onClick="${this.userLogin}">
+                Login
+              </button>`}
+        </div>
+        <div></div>
+      </div>
+    `
+  }
+}
+
+render(html` <${App} /> `, document.body)
